908b4243708a9b42ff48bbdf2befac4f58d96796
Fred Dubois <fred.dubois@gmail.com>
2011-08-08 11:54:57 -0400

    Initial import

---
+++ src/main/java/foo/bar/A.java
@@ -0,0 +1,10 @@
+package foo.bar;
+
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+public class A extends PathMatchingResourcePatternResolver {
+
+    public static void main(String[] args) {
+        System.out.println("hello!");
+    }
+}
